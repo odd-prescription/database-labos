a51abcd5078a3017dda0adc497957168d94228f8
Eahta <qwefg1059@gmail.com>
2025-09-15 21:12:27 +0800

    дополнил таблицы, действия всех внешних ключей временно - NO ACTION, переименована таблица Diagnoses, удалена PatientDistricts

--- lr_number2/dbo/Tables/Patients.sql
+++ lr_number2/dbo/Tables/Patients.sql
@@ -4,5 +4,7 @@ CREATE TABLE [dbo].[Patients]
 (
 	[PatientId] INT NOT NULL PRIMARY KEY,
 	[LastName] NVARCHAR(60) NOT NULL,
-	[FirstName] NVARCHAR(60) NOT NULL
+	[FirstName] NVARCHAR(60) NOT NULL,
+	[Gender] NVARCHAR(10),
+	[Address] NVARCHAR(80)
 )
