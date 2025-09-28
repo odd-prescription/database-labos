e988b19babd7b8ffbce971120e76329091b6cfd9
Eahta <eltenipasarof01@gmail.com>
2025-09-28 19:00:44 +0800

    lr_number_4: заполнены атрибуты таблиц

--- lr_number4/Script.sql
+++ lr_number4/Script.sql
@@ -12,40 +12,83 @@
 CREATE DATABASE [db_lr_4];
 
 CREATE TABLE [dbo].[Students] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT IDENTITY PRIMARY KEY,
+	[LastName] VARCHAR(30) NOT NULL,
+	[FirstName] VARCHAR(30) NOT NULL,
+	[Patronymic] VARCHAR(30),
+	[Biography] NVARCHAR(200),
+	[Email] VARCHAR(30) NOT NULL UNIQUE,
+	[Password] VARCHAR(16) NOT NULL CHECK(Password >= 8),
+	[Birthday] DATETIME CHECK(EXTRACT(YEAR FROM NOW()) - EXTRACT(YEAR FROM Birthday) >= 14), 
+								-- типо студенту д.б больше 14 лет
 );
 
 CREATE TABLE [dbo].[Teachers] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT IDENTITY PRIMARY KEY,
+	[LastName] VARCHAR(30) NOT NULL,
+	[FirstName] VARCHAR(30) NOT NULL,
+	[Patronymic] VARCHAR(30),
+	[Biography] NVARCHAR(200),
+	[Experience] INT CHECK(Experience > 0),
+	[Email] VARCHAR(30) NOT NULL UNIQUE,
+	[Password] VARCHAR(16) NOT NULL CHECK(Password >= 8),
+	[Birthday] DATETIME CHECK(EXTRACT(YEAR FROM NOW()) - EXTRACT(YEAR FROM Birthday) >= 18),
+		
 );
 
 CREATE TABLE [dbo].[Courses] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT PRIMARY KEY,
+	[Description] NVARCHAR(400),
+	[Price] MONEY CHECK(Price >= 0),
+	[DurationInHours] INT CHECK(DurationInHours > 0),
+	[Student] INT NOT NULL,
+	[Teacher] INT NOT NULL, 
+	FOREIGN KEY (Student) REFERENCES Students(Id),
+	FOREIGN KEY (Teacher) REFERENCES Teachers(Id)
 );
-CREATE TABLE [dbo].[Lessons] (
-	Id INT PRIMARY KEY,
 
+CREATE TABLE [dbo].[Lessons] (
+	[Id] INT PRIMARY KEY,
+	[Course] INT NOT NULL,
+	[Content] NVARCHAR(1000),
+	[VideoURL] NVARCHAR(255),
+	FOREIGN KEY (Course) REFERENCES Courses(Id),
 );
 
 CREATE TABLE [dbo].[Homeworks] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT PRIMARY KEY,
+	[Lesson] INT NOT NULL,
+	[Description] NVARCHAR(1000),
+	[DueDate] DATETIME,
+	[Score] INT CHECK (Score >= 0),
+	FOREIGN KEY (Teacher) REFERENCES Teachers(Id),
+	FOREIGN KEY (Lesson) REFERENCES Lessons(Id)
 );
 
 CREATE TABLE [dbo].[Certificates] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT IDENTITY PRIMARY KEY,
+	[UniqueCode] INT UNIQUE NOT NULL,
+	[Course] INT NOT NULL,
+	[Student] INT NOT NULL,
+	[Description] NVARCHAR(100) NOT NULL,
+	[IssueDate] DATETIME NOT NULL
 );
 
 CREATE TABLE [dbo].[Reviews] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT PRIMARY KEY,
+	[Stars] INT NOT NULL CHECK (Stars <= 5 AND Stars > 0),
+	[Student] INT NOT NULL,
+	[Description] NVARCHAR(200),
+	[Course] INT NOT NULL,
+	[CreatedAt] DATETIME NOT NULL,
+	FOREIGN KEY (Student) REFERENCES Students(Id),
+	FOREIGN KEY (Course) REFERENCES Courses(Id)
 );
 
 CREATE TABLE [dbo].[Materials] (
-	Id INT PRIMARY KEY,
-
+	[Id] INT PRIMARY KEY,
+	[Lesson] INT NOT NULL,
+	[Description] NVARCHAR(255),
+	[URL] NVARCHAR(255) NOT NULL,
+	FOREIGN KEY (Lesson) REFERENCES Lessons(Id)
 );
