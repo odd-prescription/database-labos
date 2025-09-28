6ec269a006e4e86b3a12837371776955fbc25749
Eahta <eltenipasarof01@gmail.com>
2025-09-28 20:38:29 +0800

    lr_number_4: исправлены ошибки в скрипте

--- lr_number4/Script.sql
+++ lr_number4/Script.sql
@@ -18,8 +18,8 @@ CREATE TABLE [dbo].[Students] (
 	[Patronymic] VARCHAR(30),
 	[Biography] NVARCHAR(200),
 	[Email] VARCHAR(30) NOT NULL UNIQUE,
-	[Password] VARCHAR(16) NOT NULL CHECK(Password >= 8),
-	[Birthday] DATETIME CHECK(EXTRACT(YEAR FROM NOW()) - EXTRACT(YEAR FROM Birthday) >= 14), 
+	[Password] VARCHAR(16) NOT NULL CHECK(LEN(Password) >= 8),
+	[Birthday] DATETIME CHECK(DATEDIFF(YEAR, Birthday, GETDATE()) - YEAR(Birthday) >= 14), 
 								-- типо студенту д.б больше 14 лет
 );
 
@@ -31,24 +31,22 @@ CREATE TABLE [dbo].[Teachers] (
 	[Biography] NVARCHAR(200),
 	[Experience] INT CHECK(Experience > 0),
 	[Email] VARCHAR(30) NOT NULL UNIQUE,
-	[Password] VARCHAR(16) NOT NULL CHECK(Password >= 8),
-	[Birthday] DATETIME CHECK(EXTRACT(YEAR FROM NOW()) - EXTRACT(YEAR FROM Birthday) >= 18),
-		
+	[Password] VARCHAR(16) NOT NULL CHECK(LEN(Password) >= 8),
+	[Birthday] DATETIME CHECK(DATEDIFF(YEAR, Birthday, GETDATE()) - YEAR(Birthday) >= 18),
 );
 
 CREATE TABLE [dbo].[Courses] (
-	[Id] INT PRIMARY KEY,
+	[Id] INT IDENTITY PRIMARY KEY,
+	[Title] NVARCHAR(255) NOT NULL UNIQUE,
 	[Description] NVARCHAR(400),
 	[Price] MONEY CHECK(Price >= 0),
-	[DurationInHours] INT CHECK(DurationInHours > 0),
-	[Student] INT NOT NULL,
+	[DurationInHours] INT CHECK([DurationInHours] > 0),
 	[Teacher] INT NOT NULL, 
-	FOREIGN KEY (Student) REFERENCES Students(Id),
 	FOREIGN KEY (Teacher) REFERENCES Teachers(Id)
 );
 
 CREATE TABLE [dbo].[Lessons] (
-	[Id] INT PRIMARY KEY,
+	[Id] INT IDENTITY PRIMARY KEY,
 	[Course] INT NOT NULL,
 	[Content] NVARCHAR(1000),
 	[VideoURL] NVARCHAR(255),
@@ -56,12 +54,11 @@ CREATE TABLE [dbo].[Lessons] (
 );
 
 CREATE TABLE [dbo].[Homeworks] (
-	[Id] INT PRIMARY KEY,
+	[Id] INT IDENTITY PRIMARY KEY,
 	[Lesson] INT NOT NULL,
 	[Description] NVARCHAR(1000),
 	[DueDate] DATETIME,
-	[Score] INT CHECK (Score >= 0),
-	FOREIGN KEY (Teacher) REFERENCES Teachers(Id),
+	[MaxScore] INT CHECK (MaxScore >= 0),
 	FOREIGN KEY (Lesson) REFERENCES Lessons(Id)
 );
 
@@ -71,24 +68,146 @@ CREATE TABLE [dbo].[Certificates] (
 	[Course] INT NOT NULL,
 	[Student] INT NOT NULL,
 	[Description] NVARCHAR(100) NOT NULL,
-	[IssueDate] DATETIME NOT NULL
+	[IssueDate] DATETIME NOT NULL,
+	FOREIGN KEY (Course) REFERENCES Courses(Id),
+	FOREIGN KEY (Student) REFERENCES Students(Id)
 );
 
 CREATE TABLE [dbo].[Reviews] (
-	[Id] INT PRIMARY KEY,
+	[Id] INT IDENTITY  PRIMARY KEY,
 	[Stars] INT NOT NULL CHECK (Stars <= 5 AND Stars > 0),
 	[Student] INT NOT NULL,
 	[Description] NVARCHAR(200),
 	[Course] INT NOT NULL,
-	[CreatedAt] DATETIME NOT NULL,
+	[CreatedAt] DATETIME NOT NULL DEFAULT GETDATE()
 	FOREIGN KEY (Student) REFERENCES Students(Id),
 	FOREIGN KEY (Course) REFERENCES Courses(Id)
 );
 
 CREATE TABLE [dbo].[Materials] (
-	[Id] INT PRIMARY KEY,
+	[Id] INT IDENTITY PRIMARY KEY,
 	[Lesson] INT NOT NULL,
 	[Description] NVARCHAR(255),
 	[URL] NVARCHAR(255) NOT NULL,
 	FOREIGN KEY (Lesson) REFERENCES Lessons(Id)
 );
+
+INSERT INTO [dbo].[Students] (
+	[LastName],
+	[FirstName],
+	[Patronymic],
+	[Biography],
+	[Email],
+	[Password],
+	[Birthday]
+) VALUES (
+	'Фельков',
+	'Гойда',
+	'Патронович',
+	'Родился в городе Донецк',
+	'goyda2014@yandex.ru',
+	'1234567810',
+	'01/01/2006'),
+	(
+	'Дмитриев',
+	'Антон',
+	'Добросалович',
+	'',
+	'genshinismylive@mail.ru',
+	'gs^3habd3h',
+	'24/03/2001'
+	)
+
+INSERT INTO [dbo].[Teachers] (
+	[LastName],
+	[FirstName],
+	[Patronymic],
+	[Biography],
+	[Experience],
+	[Email],
+	[Password],
+	[Birthday]
+) VALUES (
+	'Назарбаев',
+	'Асымжарты',
+	'Чумчури-оглы-Ахмет',
+	'Профессианальный повар с 13-летним стажем',
+	'12',
+	'asamgartjy@gmail.com',
+	'12345678910',
+	'19/04/1994'
+)
+
+INSERT INTO [dbo].[Courses] (
+	[Title],
+	[Description],
+	[Price],
+	[DurationInHours] ,
+	[Teacher]
+) VALUES (
+	'Обучение началам французской кухни',
+	'Вместе приготовим крем-брюле, лягушачьи ножки и луковый суп',
+	'1999.99',
+	'12',
+	'1'
+)
+
+INSERT INTO [dbo].[Lessons] (
+	[Course],
+	[Content],
+	[VideoURL]
+) VALUES (
+	'1',
+	'содержание',
+	'https://www.youtube.com/watch?v=WePNs-G7puA'
+)
+
+INSERT INTO [dbo].[Homeworks] (
+	[Lesson],
+	[Description],
+	[DueDate],
+	[MaxScore]
+) VALUES (
+	'1',
+	'Приготовьте крем-брюле по методикам, описанным в уроке',
+	'09/09/2025',
+	'2'
+)
+
+INSERT INTO [dbo].[Certificates] (
+	[UniqueCode],
+	[Course],
+	[Student],
+	[Description],
+	[IssueDate]
+) VALUES (
+	'1337437',
+	'1',
+	'1',
+	'Хорошо поработал',
+	'20/10/2099'
+)
+
+INSERT INTO [dbo].[Reviews] (
+	[Stars],
+	[Student],
+	[Description],
+	[Course],
+	[CreatedAt]
+) VALUES (
+	'5',
+	'1',
+	'Мне понравилось, курс отлчается от других тем, что он единственный в своём роде',
+	'1',
+	'21/10/2025'
+)
+
+INSERT INTO [dbo].[Materials] (
+	[Lesson],
+	[Description],
+	[URL]
+) VALUES (
+	'1',
+	'',
+	'https://www.youtube.com/watch?v=jVFvXKDKCZg&pp=0gcJCfYJAYcqIYzv'
+)
