1494c52858bb93a11824ccc390df8b8b4f109373
Eahta <eltenipasarof01@gmail.com>
2025-09-28 18:28:22 +0800

    lr_number_4: созданы таблицы

---
+++ lr_number4/Script.sql
@@ -0,0 +1,51 @@
+﻿/* 
+	База данных онлайн-курсов.
+	Таблицы: Курсы, 
+			Преподаватели, 
+			Студенты, 
+			Уроки, 
+			Домашние задания, 
+			Сертификаты, 
+			Отзывы, 
+			Материалы
+*/
+CREATE DATABASE [db_lr_4];
+
+CREATE TABLE [dbo].[Students] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Teachers] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Courses] (
+	Id INT PRIMARY KEY,
+
+);
+CREATE TABLE [dbo].[Lessons] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Homeworks] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Certificates] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Reviews] (
+	Id INT PRIMARY KEY,
+
+);
+
+CREATE TABLE [dbo].[Materials] (
+	Id INT PRIMARY KEY,
+
+);
