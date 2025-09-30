1210785392de0ef10300978cdc4a03e1159c40d3
Eahta <eltenipasarof01@gmail.com>
2025-09-30 20:34:17 +0800

    добавлен скрипт вставки с данными

---
+++ lr_number5/InsertingScript.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [dbo].[Restaurants] ([Name], [Address], [PhoneNumber], [Capacity]) VALUES
+('La Bella Vita', 'ул. Ленина 10, Москва', '+79991234567', 50),
+('Sushi Haven', 'пр. Мира 25, Санкт-Петербург', '+79992345678', 30),
+('Burger Bonanza', 'ул. Победы 5, Казань', '+79993456789', 40);
+
