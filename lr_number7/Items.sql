71d724980fe55a2f46b49abfbb05e45107c0af14
Eahta <eltenipasarof01@gmail.com>
2025-10-02 20:14:21 +0800

    изменены атрибуты таблиц

--- lr_number7/Items.sql
+++ lr_number7/Items.sql
@@ -1,8 +1,13 @@
 ﻿CREATE TABLE [dbo].[Items]
 (
 	[ItemId] NVARCHAR(10) NOT NULL PRIMARY KEY,
-	[Name] NVARCHAR(100) NOT NULL,
+	[ArmorId] NVARCHAR(10),
+	[WeaponId] NVARCHAR(10),
+	[Name] NVARCHAR(100) NOT NULL DEFAULT 'ITEM_DEFAULT',
 	[Description] NVARCHAR(200),
 	[Price] INT NOT NULL CHECK([Price] >= 0),
-	[Weight] INT NOT NULL CHECK([Weight] >= 0)
+	[Weight] DECIMAL(2, 2) NOT NULL CHECK([Weight] >= 0),
+	FOREIGN KEY ([ArmorId]) REFERENCES [Armors]([ArmorId]),
+	FOREIGN KEY ([WeaponId]) REFERENCES [Weapons]([WeaponId]),
+
 )
--- lr_number7/Weapons.sql
+++ lr_number7/Weapons.sql
@@ -2,5 +2,6 @@
 (
 	[WeaponId] NVARCHAR(10) NOT NULL PRIMARY KEY,
 	[Damage] INT NOT NULL CHECK([Damage] >= 0),
-	[Material] NVARCHAR(20) NOT NULL
+	[Material] NVARCHAR(20) NOT NULL,
+	[Spell] NVARCHAR(50),
 )
