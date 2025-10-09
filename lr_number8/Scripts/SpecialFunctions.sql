d309258d58982e831c93796f017ea42c8f55b2a5
Eahta <eltenipasarof01@gmail.com>
2025-10-09 16:32:04 +0800

    добавлены новые скрипты

---
+++ lr_number8/Scripts/SpecialFunctions.sql
@@ -0,0 +1,25 @@
+﻿/*  Преобразования CAST CONVERT
+	Математические функции
+	Строковые функции  */
+USE db_lr_8;
+GO
+
+SELECT CAST([Id] AS DECIMAL(2)) AS [CastedId],
+	[CreatedAt],
+	CONVERT(NVARCHAR(20), [CreatedAt], 103) AS [ConvertedDate]
+	FROM [Reviews]; -- 103 - dd/mm/yyyy
+
+SELECT [Title],
+	[Price],
+	POWER([Price], 3) AS [PoweredCoursePrice],
+	ROUND([Price], 1) AS [RoundedCoursePrice],
+	RAND() AS [JustRandomNumber],
+	[DurationInHours],
+	SQRT([DurationInHours]) AS [SquaredDuration],
+	ABS([DurationInHours]) AS [AbsoluteDuration]
+	FROM [Courses];
+
+SELECT CONCAT([LastName], ' ' ,[FirstName], ' ', [Patronymic]) AS [FullName],
+	LEN(CONCAT([LastName], ' ' ,[FirstName], ' ', [Patronymic])) AS [FullNameLength],
+	LEFT([Biography], 10) AS [BioLeftPart]
+	FROM [Students];
---
+++ lr_number8/Scripts/Subqueries.sql
@@ -0,0 +1,7 @@
+﻿-- Подзапросы
+USE db_lr_8;
+GO
+SELECT [LastName], [FirstName] FROM [Teachers]
+WHERE [Id] = (
+	SELECT TOP 1 [Teacher] FROM [Courses] WHERE [DurationInHours] > 30
+);
