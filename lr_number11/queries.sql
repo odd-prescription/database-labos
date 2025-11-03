97e5f9728c99f74b85410980d68307f7e6f3ed4e
Eahta <eltenipasarof01@gmail.com>
2025-11-03 17:08:02 +0800

    добавлены дополнительные выборки

--- lr_number11/queries.sql
+++ lr_number11/queries.sql
@@ -21,3 +21,12 @@ EXCEPT SELECT [LastName] FROM [Employees];
 SELECT [LastName] AS [TotalHumans] FROM [Clients]
 UNION ALL SELECT [LastName] AS [Humans] FROM [Employees];
 
+SELECT AVG([Price]) AS [AverageAlcohol] FROM [Menu]
+WHERE [Category] = (SELECT [Id] FROM [DishCategories] 
+					WHERE [Title] = 'Алкоголь');
+
+SELECT CONCAT([C].[LastName], ' ', [C].[FirstName]) AS [ClientsAboveAverage] FROM [Clients] AS [C]
+INNER JOIN [Orders] AS [O] ON [O].[ClientId] = [C].[Id]
+INNER JOIN [Payments] AS [P] ON [P].[OrderId] = [O].[Id]
+GROUP BY CONCAT([C].[LastName], ' ', [C].[FirstName])
+HAVING SUM([P].[Amount]) < (SELECT(AVG([Amount])) FROM [Payments]);
