c7ca1b473f8892852e19440b1e6c63baba036da5
Eahta <eltenipasarof01@gmail.com>
2025-11-03 18:42:14 +0800

    добавлен скрипт создания таблиц

---
+++ lr_number12/creating-tables.sql
@@ -0,0 +1,61 @@
+-- ТЕма: Библиотека
+USE [db_lr_12];
+GO
+
+CREATE TABLE [Visitors] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[LastName] NVARCHAR(100) NOT NULL,
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[Address] NVARCHAR(100) NOT NULL,
+	[PhoneNumber] NVARCHAR(40) NOT NULL,
+	[Email] NVARCHAR(60),
+	[Birthday] DATE NOT NULL CHECK(CONVERT(INT, DATEDIFF(YEAR, [Birthday], GETDATE()) / 365) > 6)
+);
+CREATE TABLE [Authors] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[LastName] NVARCHAR(100) NOT NULL,
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[MiddleName] NVARCHAR(100),
+	[Birthday] DATE,
+	[Country] NVARCHAR(100)
+);
+CREATE TABLE [Genres] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[Title] NVARCHAR(50)
+);
+CREATE TABLE [PublishingCompanies] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[Title] NVARCHAR(100) NOT NULL,
+	[Address] NVARCHAR(100) NOT NULL,
+	[PhoneNumber] NVARCHAR(40) NOT NULL,
+);
+CREATE TABLE [Books] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[Title] NVARCHAR(100) NOT NULL,
+	[Description] NVARCHAR(200),
+	[AuthorId] INT NOT NULL,
+	[GenreId] INT NOT NULL,
+	[PublishingCompanyId] INT NOT NULL,
+	[ReleaseDate] DATE DEFAULT GETDATE(),
+	FOREIGN KEY ([AuthorId]) REFERENCES [Authors]([Id]),
+	FOREIGN KEY ([GenreId]) REFERENCES [Genres]([Id]),
+	FOREIGN KEY ([PublishingCompanyId]) REFERENCES [PublishingCompanies]([Id]),
+);
+CREATE TABLE [BookIssuances] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[VisitorId] INT NOT NULL,
+	[BookId] INT NOT NULL,
+	[Since] DATETIME NOT NULL,
+	[Until] DATETIME NOT NULL,
+	[Status] BIT DEFAULT 0,
+	FOREIGN KEY ([VisitorId]) REFERENCES [Visitors]([Id]),
+	FOREIGN KEY ([BookId]) REFERENCES [Books]([Id]),
+);
+CREATE TABLE [Penalties] (
+	[Id] INT IDENTITY PRIMARY KEY,
+	[IssuanceId] INT NOT NULL,
+	[Amount] MONEY NOT NULL CHECK([Amount] > 0),
+	[AccrualDate] DATETIME NOT NULL,
+	[Status] BIT DEFAULT 0,
+	FOREIGN KEY ([IssuanceId]) REFERENCES [BookIssuances]([Id]),
+);
